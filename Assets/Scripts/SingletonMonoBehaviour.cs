60b59fe65367b12039d2b1a031984810ed1df455
DESKTOP-FTQ7RG4\vantan <katoon.katudon@gmail.com>
2022-01-17 23:10:50 +0900

    singletonmonobehaviourを変更

--- Assets/Scripts/SingletonMonoBehaviour.cs
+++ Assets/Scripts/SingletonMonoBehaviour.cs
@@ -5,15 +5,40 @@ using UnityEngine;
 public class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
 {
     private static T instance;
+
     public static T Instance
     {
         get
         {
-            if (instance == null)
+            if (instance is null)
             {
                 instance = (T)FindObjectOfType(typeof(T));
+
+                if (instance is null)
+                {
+                    Debug.LogError(typeof(T) + "がシーンに存在しません。");
+                }
             }
             return instance;
         }
+    }
+
+    protected virtual void Awake()
+    {
+        Check();
+    }
+
+    void Check()
+    {
+        if (instance == null)
+        {
+            instance = this as T;
+            return;
+        }
+        else if (Instance == this)
+        {
+            return;
+        }
+        Destroy(this);
     }
 }
