58e67a905a427797691b9923c58f7c1f7222a682
DESKTOP-FTQ7RG4\vantan <katoon.katudon@gmail.com>
2022-01-19 19:12:56 +0900

    敵用のバトル時のstatemachineの作成開始

---
+++ Assets/Scripts/BattleEnemyStateMachine.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using State = StateMachine<BattleEnemyStateMachine>.State;
+
+public class BattleEnemyStateMachine : MonoBehaviour
+{
+    StateMachine<BattleEnemyStateMachine> m_stateMachine;
+    enum ActEvent : byte
+    {
+        Wait,
+        Standby,
+        Move,
+        BattleAction,
+        //ActionEnd,
+        Dead
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
