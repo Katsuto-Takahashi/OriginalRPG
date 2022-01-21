c5e522567c88c9af0add1015ccda44c187a404cd
DESKTOP-FTQ7RG4\vantan <katoon.katudon@gmail.com>
2022-01-21 22:37:36 +0900

    敵のstatemachineを追加

---
+++ Assets/Scripts/BattleEnemyState.cs
@@ -0,0 +1,293 @@
+﻿using UnityEngine;
+using State = StateMachine<BattleEnemyStateMachine>.State;
+
+public partial class BattleEnemyStateMachine : MonoBehaviour
+{
+    public class BattleEnemyState
+    {
+        public class Wait : State
+        {
+            protected override void OnEnter(State prevState)
+            {
+                Debug.Log("Wait");
+            }
+            protected override void OnUpdate()
+            {
+                if (owner.m_isDead)
+                {
+                    StateMachine.Dispatch((int)ActEvent.Dead);
+                }
+                else if (owner.m_isBind)
+                {
+                    StateMachine.Dispatch((int)ActEvent.Bind);
+                }
+                else if (!owner.m_isBattle)
+                {
+                    StateMachine.Dispatch((int)ActEvent.NoBattle);
+                }
+                if (owner.m_actionCount > 0)
+                {
+                    StateMachine.Dispatch((int)ActEvent.Standby);
+                }
+            }
+            protected override void OnExit(State nextState)
+            {
+            }
+        }
+
+        public class Standby : State
+        {
+            protected override void OnEnter(State prevState)
+            {
+                Debug.Log("Standby");
+                owner.m_targetIndex = Random.Range(0, owner.m_targets.Count);
+                owner.m_targetPosition = owner.m_targets[owner.m_targetIndex].transform.position;
+                owner.m_distance = (owner.m_targetPosition.x - owner.transform.position.x) * (owner.m_targetPosition.x - owner.transform.position.x) + (owner.m_targetPosition.z - owner.transform.position.z) * (owner.m_targetPosition.z - owner.transform.position.z);
+            }
+
+            protected override void OnUpdate()
+            {
+                if (owner.m_isDead)
+                {
+                    StateMachine.Dispatch((int)ActEvent.Dead);
+                }
+                else if (owner.m_isBind)
+                {
+                    StateMachine.Dispatch((int)ActEvent.Bind);
+                }
+                else if (!owner.m_isBattle)
+                {
+                    StateMachine.Dispatch((int)ActEvent.NoBattle);
+                }
+                else if (owner.m_distance > 0)
+                {
+                    StateMachine.Dispatch((int)ActEvent.Move);
+                }
+                else
+                {
+                    StateMachine.Dispatch((int)ActEvent.BattleAction);
+                }
+            }
+
+            protected override void OnExit(State nextState)
+            {
+                if (nextState is Move || nextState is BattleAction)
+                {
+                    owner.m_actionCount--;
+                }
+            }
+        }
+
+        public class Move : State
+        {
+            protected override void OnEnter(State prevState)
+            {
+                Debug.Log("Move");
+                owner.m_stop.Value = true;
+                owner.PlayAnimation("Run");
+            }
+
+            protected override void OnUpdate()
+            {
+                owner.m_distance = (owner.m_targetPosition.x - owner.transform.position.x) * (owner.m_targetPosition.x - owner.transform.position.x) + (owner.m_targetPosition.z - owner.transform.position.z) * (owner.m_targetPosition.z - owner.transform.position.z);
+                if (owner.m_isDead)
+                {
+                    StateMachine.Dispatch((int)ActEvent.Dead);
+                }
+                else if (owner.m_isBind)
+                {
+                    StateMachine.Dispatch((int)ActEvent.Bind);
+                }
+                else if (!owner.m_isBattle)
+                {
+                    StateMachine.Dispatch((int)ActEvent.NoBattle);
+                }
+                //else if (true)
+                //{
+                //    StateMachine.Dispatch((int)ActEvent.Wait);
+                //}
+                if (owner.m_distance > owner.m_normalSkill[0].AttackRange)
+                {
+                    Vector3 target = owner.m_targetPosition;
+                    target.y = 0f;
+                    owner.transform.position = new Vector3(owner.transform.position.x, 0f, owner.transform.position.z);
+                    owner.transform.LookAt(target);
+                    owner.transform.position = Vector3.MoveTowards(owner.transform.position, owner.m_targetPosition, owner.m_moveSpeed * Time.deltaTime);
+                }
+                else
+                {
+                    Vector3 target = owner.m_targetPosition;
+                    target.y = 0f;
+                    owner.transform.position = new Vector3(owner.transform.position.x, 0f, owner.transform.position.z);
+                    owner.transform.LookAt(target);
+                    StateMachine.Dispatch((int)ActEvent.BattleAction);
+                }
+            }
+
+            protected override void OnExit(State nextState)
+            {
+                if (nextState is Wait)
+                {
+                    owner.m_stop.Value = false;
+                }
+            }
+        }
+
+        public class BattleAction : State
+        {
+            protected override void OnEnter(State prevState)
+            {
+                Debug.Log("BattleAction");
+            }
+
+            protected override void OnUpdate()
+            {
+                if (owner.FinishedAnimation())
+                {
+                    StateMachine.Dispatch((int)ActEvent.ActionEnd);
+                }
+            }
+
+            protected override void OnExit(State nextState)
+            {
+            }
+        }
+
+        public class ActionEnd : State
+        {
+            protected override void OnEnter(State prevState)
+            {
+                Debug.Log("ActionEnd");
+                owner.m_stop.Value = false;
+            }
+
+            protected override void OnUpdate()
+            {
+                if (owner.m_isDead)
+                {
+                    StateMachine.Dispatch((int)ActEvent.Dead);
+                }
+                else if (owner.m_isBind)
+                {
+                    StateMachine.Dispatch((int)ActEvent.Bind);
+                }
+                else if (!owner.m_isBattle)
+                {
+                    StateMachine.Dispatch((int)ActEvent.NoBattle);
+                }
+                else
+                {
+                    StateMachine.Dispatch((int)ActEvent.Wait);
+                }
+            }
+
+            protected override void OnExit(State nextState)
+            {
+            }
+        }
+
+        public class Bind : State
+        {
+            protected override void OnEnter(State prevState)
+            {
+                Debug.Log("Bind");
+                owner.m_stop.Value = true;
+                owner.PlayAnimation("");
+            }
+
+            protected override void OnUpdate()
+            {
+                if (owner.m_isBind)
+                {
+                    if (owner.m_isDead)
+                    {
+                        StateMachine.Dispatch((int)ActEvent.Dead);
+                    }
+                    else if (!owner.m_isBattle)
+                    {
+                        StateMachine.Dispatch((int)ActEvent.NoBattle);
+                    }
+                }
+                else
+                {
+                    if (owner.m_isBattle)
+                    {
+                        StateMachine.Dispatch((int)ActEvent.Wait);
+                    }
+                    else
+                    {
+                        StateMachine.Dispatch((int)ActEvent.NoBattle);
+                    }
+                }
+            }
+
+            protected override void OnExit(State nextState)
+            {
+                owner.m_stop.Value = false;
+            }
+        }
+
+        public class NoBattle : State
+        {
+            protected override void OnEnter(State prevState)
+            {
+                Debug.Log("NoBattle");
+                owner.m_stop.Value = false;
+                owner.m_isBattle = false;
+                owner.m_currentTimer = 0f;
+                owner.m_actionCount = 0;
+            }
+
+            protected override void OnUpdate()
+            {
+                if (owner.m_isDead)
+                {
+                    StateMachine.Dispatch((int)ActEvent.Dead);
+                }
+                else if (owner.m_isBattle)
+                {
+                    StateMachine.Dispatch((int)ActEvent.Wait);
+                }
+            }
+
+            protected override void OnExit(State nextState)
+            {
+            }
+        }
+
+        public class Dead : State
+        {
+            protected override void OnEnter(State prevState)
+            {
+                Debug.Log("Dead");
+                owner.m_actionCount = 0;
+                owner.m_stop.Value = true;
+                owner.PlayAnimation("");
+            }
+
+            protected override void OnUpdate()
+            {
+                if (!owner.m_isDead)
+                {
+                    if (owner.m_isBattle)
+                    {
+                        StateMachine.Dispatch((int)ActEvent.Wait);
+                    }
+                    else
+                    {
+                        StateMachine.Dispatch((int)ActEvent.NoBattle);
+                    }
+                }
+            }
+
+            protected override void OnExit(State nextState)
+            {
+                owner.PlayAnimation("");
+                if (owner.FinishedAnimation())
+                {
+                    owner.m_stop.Value = false;
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/BattleEnemyStateMachine.cs
+++ Assets/Scripts/BattleEnemyStateMachine.cs
@@ -1,27 +1,142 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
 using State = StateMachine<BattleEnemyStateMachine>.State;
 
-public class BattleEnemyStateMachine : MonoBehaviour
+public partial class BattleEnemyStateMachine : MonoBehaviour
 {
     StateMachine<BattleEnemyStateMachine> m_stateMachine;
+
     enum ActEvent : byte
     {
         Wait,
         Standby,
         Move,
         BattleAction,
-        //ActionEnd,
+        ActionEnd,
+        Bind,
+        NoBattle,
         Dead
     }
+
+    Animator m_animator;
+
+    /// <summary>戦闘中かどうか</summary>
+    bool m_isBattle = false;
+    /// <summary>戦闘中かのフラグ</summary>
+    public bool IsBattle { get => m_isBattle; set => m_isBattle = value; }
+
+    /// <summary>死亡しているかどうか</summary>
+    bool m_isDead = false;
+    /// <summary>死亡しているかのフラグ</summary>
+    public bool IsDead { get => m_isDead; set => m_isDead = value; }
+
+    /// <summary>入力による動きを止めるかどうか</summary>
+    BoolReactiveProperty m_stop = new BoolReactiveProperty(false);
+    /// <summary>入力による動きを止めるかのフラグ</summary>
+    public IReactiveProperty<bool> Stop => m_stop;
+
+    /// <summary>バインドされているかどうか</summary>
+    bool m_isBind = false;
+    /// <summary>バインドされているかのフラグ</summary>
+    public bool IsBind { get => m_isBind; set => m_isBind = value; }
+
+    /// <summary>対象との距離</summary>
+    float m_distance;
+    /// <summary>現在の待機時間</summary>
+    float m_currentTimer;
+
+    /// <summary>行動の対象</summary>
+    List<GameObject> m_targets = new List<GameObject>();
+    /// <summary>行動の対象List</summary>
+    public List<GameObject> Targets { get => m_targets; set => m_targets = value; }
+
+    /// <summary>対象のIndex</summary>
+    int m_targetIndex;
+    /// <summary>対象のPosition</summary>
+    Vector3 m_targetPosition;
+    /// <summary>行動回数</summary>
+    int m_actionCount;
+    /// <summary>蓄積可能な行動回数</summary>
+    int m_maxActionCount;
+
+    #region パラメーター
+    /// <summary>現在のHP</summary>
+    int m_nowHP;
+    /// <summary>最大のHP</summary>
+    int m_hp;
+    /// <summary>現在のAP</summary>
+    int m_nowAP;
+    /// <summary>最大のAP</summary>
+    int m_ap;
+    /// <summary>物理攻撃力</summary>
+    int m_strength;
+    /// <summary>物理防御力</summary>
+    int m_defense;
+    /// <summary>魔法攻撃力</summary>
+    int m_magicPower;
+    /// <summary>魔法防御力</summary>
+    int m_magicResist;
+
+
+    /// <summary>移動速度</summary>
+    float m_moveSpeed;
+    /// <summary>待機時間</summary>
+    float m_actionTimer;
+    #endregion
+
+    /// <summary>通常攻撃</summary>
+    List<SkillData> m_normalSkill = new List<SkillData>();
+    /// <summary>スキル</summary>
+    List<SkillData> m_skillDatas = new List<SkillData>();
+    /// <summary>魔法</summary>
+    List<SkillData> m_magicDatas = new List<SkillData>();
+
     void Start()
     {
-        
+        m_stateMachine = new StateMachine<BattleEnemyStateMachine>(this);
+
+        m_stateMachine.AddAnyTransition<BattleEnemyState.Wait>((int)ActEvent.Wait);
+        m_stateMachine.AddAnyTransition<BattleEnemyState.Standby>((int)ActEvent.Standby);
+        m_stateMachine.AddAnyTransition<BattleEnemyState.Move>((int)ActEvent.Move);
+        m_stateMachine.AddAnyTransition<BattleEnemyState.BattleAction>((int)ActEvent.BattleAction);
+        m_stateMachine.AddAnyTransition<BattleEnemyState.ActionEnd>((int)ActEvent.ActionEnd);
+        m_stateMachine.AddAnyTransition<BattleEnemyState.Bind>((int)ActEvent.Bind);
+        m_stateMachine.AddAnyTransition<BattleEnemyState.NoBattle>((int)ActEvent.NoBattle);
+        m_stateMachine.AddAnyTransition<BattleEnemyState.Dead>((int)ActEvent.Dead);
+
+        m_stateMachine.Start<BattleEnemyState.NoBattle>();
     }
 
     void Update()
     {
-        
+        Timer(m_stateMachine.CurrentSate);
+    }
+
+    void Timer(State state)
+    {
+        if (state is BattleEnemyState.Wait || state is BattleEnemyState.Standby)
+        {
+            if (m_actionCount < m_maxActionCount)
+            {
+                m_currentTimer += Time.deltaTime;
+            }
+            if (m_currentTimer > m_actionTimer)
+            {
+                m_currentTimer -= m_actionTimer;
+                m_actionCount++;
+            }
+        }
+    }
+
+    bool FinishedAnimation(int layer = 0)
+    {
+        return AnimationController.Instance.FinishedAnimation(m_animator, layer);
+    }
+
+    void PlayAnimation(string stateName, float transitionDuration = 0.1f)
+    {
+        AnimationController.Instance.PlayAnimation(m_animator, stateName, transitionDuration);
     }
 }
