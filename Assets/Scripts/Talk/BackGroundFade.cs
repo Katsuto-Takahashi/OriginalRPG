99ece2533b602040bea6e3c84b95732ccfed658f
DESKTOP-FTQ7RG4\vantan <katoon.katudon@gmail.com>
2022-01-28 21:43:56 +0900

    会話用のscript作成開始

---
+++ Assets/Scripts/Talk/BackGroundFade.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundFade : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    IEnumerator FadeIn()
+    {
+        yield return null;
+    }
+    IEnumerator FadeOut()
+    {
+        yield return null;
+    }
+
+    IEnumerator LeftFadeIn()
+    {
+        yield return null;
+    }
+    IEnumerator LeftFadeOut()
+    {
+        yield return null;
+    }
+
+    IEnumerator RightFadeIn()
+    {
+        yield return null;
+    }
+    IEnumerator RightFadeOut()
+    {
+        yield return null;
+    }
+
+    IEnumerator DownFadeIn()
+    {
+        yield return null;
+    }
+    IEnumerator DownFadeOut()
+    {
+        yield return null;
+    }
+
+    IEnumerator RisingFadeIn()
+    {
+        yield return null;
+    }
+    IEnumerator RisingFadeOut()
+    {
+        yield return null;
+    }
+}
---
+++ Assets/Scripts/Talk/MessageWindow.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MessageWindow : MonoBehaviour
+{
+    [SerializeField] Text m_messageText = null;
+    //[SerializeField] Text m_nameText = null;
+    [SerializeField] float m_waitTime = 0f;
+    [SerializeField] List<string> m_message = new List<string>();
+    int m_count;
+    bool m_skipMessage;
+    bool m_nextMessage;
+
+    bool m_stop;
+    bool m_click;
+    bool m_skipMode;
+    bool m_autoMode;
+    void Start()
+    {
+        m_count = 0;
+        MessageWindows(m_message);
+    }
+
+    void Update()
+    {
+        if (!Stop())
+        {
+            if (Click() && m_count < m_message.Count)
+            {
+                m_skipMessage = true;
+            }
+            if (m_skipMessage || Auto())
+            {
+                if (Click() || Auto())
+                {
+                    m_nextMessage = true;
+                }
+            }
+            if (m_click)
+            {
+                m_click = false;
+            }
+        }
+    }
+
+    bool Click()
+    {
+        return m_click || Skip();
+    }
+
+    bool Skip()
+    {
+        return m_skipMode;
+    }
+
+    bool Auto()
+    {
+        return m_autoMode;
+    }
+
+    bool Stop()
+    {
+        return m_stop = Input.GetKey(KeyCode.Space);
+    }
+
+    public void MessageWindows(List<string> messageList)
+    {
+        StartCoroutine(Message(messageList));
+    }
+
+    IEnumerator Message(List<string> messageList)
+    {
+        m_nextMessage = false;
+        m_skipMessage = false;
+        int wordCount = 0;
+        m_messageText.text = "";
+        while (messageList[m_count].Length > wordCount)
+        {
+            if (m_skipMessage)
+            {
+                m_messageText.text = "";
+                m_messageText.text = messageList[m_count];
+                break;
+            }
+            m_messageText.text += messageList[m_count][wordCount];
+            wordCount++;
+            yield return new WaitForSeconds(m_waitTime);
+        }
+        
+        m_count++;
+        StartCoroutine(WaitMessage(messageList));
+    }
+
+    IEnumerator WaitMessage(List<string> messageList)
+    {
+        m_nextMessage = false;
+        while (!m_nextMessage)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        if (m_count < m_message.Count)
+        {
+            MessageWindows(messageList);
+        }
+        else
+        {
+            //話が終わった後の処理
+            yield return new WaitForSeconds(2f);
+            m_count = 0;
+            MessageWindows(messageList);
+        }
+    }
+
+    public void OnClick()
+    {
+        if (m_click) m_click = false;
+        else m_click = true;
+    }
+
+    public void OnSkip()
+    {
+        if (m_skipMode) m_skipMode = false;
+        else m_skipMode = true;
+    }
+
+    public void OnAuto()
+    {
+        if (m_autoMode) m_autoMode = false;
+        else m_autoMode = true;
+    }
+
+    public IEnumerator FadeIn()
+    {
+        while (!m_skipMessage)
+        {
+            yield return null;
+        }
+    }
+    public IEnumerator FadeOut()
+    {
+        while (!m_skipMessage)
+        {
+            yield return null;
+        }
+    }
+
+    //１キャラを出しながらテキスト
+    //キャラを順に出しながらテキスト
+    //キャラを同時に出しながらテキスト
+    //１キャラを出してからテキスト
+    //キャラを順に出してからテキスト
+    //キャラを同時に出してからテキスト
+    //１キャラを消しながらテキスト
+    //キャラを順に消しながらテキスト
+    //キャラを同時に消しながらテキスト
+    //１キャラを消してからテキスト
+    //キャラを順に消してからテキスト
+    //キャラを同時に消してからテキスト
+}
