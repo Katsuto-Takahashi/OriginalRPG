07fbb7d7e218e183d3b9bf5c0440f3edc91c82b7
DESKTOP-FTQ7RG4\vantan <katoon.katudon@gmail.com>
2022-02-09 21:01:54 +0900

    btの続き

--- Assets/Scripts/BehaviorTree/BehaviorTree.cs
+++ Assets/Scripts/BehaviorTree/BehaviorTree.cs
@@ -2,231 +2,233 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BehaviorTree :MonoBehaviour
+namespace BehaviorTree
 {
-    [SerializeField]
-    protected Node[] m_nodes = new Node[] { };
-}
-
-public interface IReturnResult
-{
-    NodeState Result();
-}
-
-[System.Serializable]
-public class Node : IReturnResult, IGetNode
-{
-    [SerializeReference, SubclassSelector]
-    List<Node> m_childNode = new List<Node>();
-
-    public List<Node> ChildNode => m_childNode;
-
-    protected NodeState m_currentState;
-    public NodeState CurrentState { get => m_currentState; }
-
-    public virtual NodeState Result()
+    public interface IReturnResult
     {
-        return m_currentState;
+        NodeState Result();
     }
 
-    public virtual Node GetNode()
+    public interface IGetNode
     {
-        return this;
+        Node GetNode();
     }
 
-    public List<Node> SetNode()
+    [System.Serializable]
+    public class Node : IReturnResult, IGetNode
     {
-        return m_childNode;
-    }
-}
+        [SerializeReference, SubclassSelector]
+        List<Node> m_childNode = new List<Node>();
 
-public enum NodeState
-{
-    Running,
-    Success,
-    Failure
-}
+        public List<Node> ChildNode => m_childNode;
 
-public class BTAction : Node
-{
-    public override NodeState Result()
-    {
-        return Act();
-    }
+        protected NodeState m_currentState;
+        public NodeState CurrentState { get => m_currentState; }
 
-    /// <summary>実行内容</summary>
-    /// <returns>baseはFailure</returns>
-    protected virtual NodeState Act()
-    {
-        return NodeState.Failure;
-    }
-}
-
-public class BTConditional : Node
-{
-    public override NodeState Result()
-    {
-        return Check();
-    }
-
-    /// <summary>条件判定</summary>
-    /// <returns>baseはFailure</returns>
-    protected virtual NodeState Check()
-    {
-        return NodeState.Failure;
-    }
-}
-
-public class BTSelector : Node
-{
-    protected List<Node> m_childNodes = new List<Node>();
-    public List<Node> ChildNodes => m_childNodes;
-
-    public BTSelector AddChild()
-    {
-        m_childNodes = ChildNode;
-        return this;
-    }
-
-    public override NodeState Result()
-    {
-        for (int i = 0; i < m_childNodes.Count; i++)
-        {
-            switch (m_childNodes[i].Result())
-            {
-                case NodeState.Running:
-                    m_currentState = NodeState.Running;
-                    return CurrentState;
-                case NodeState.Success:
-                    m_currentState = NodeState.Success;
-                    return CurrentState;
-                case NodeState.Failure:
-                    continue;
-                default:
-                    break;
-            }
-        }
-        m_currentState = NodeState.Failure;
-        return CurrentState;
-    }
-}
-
-//[System.Serializable]
-public class BTSequence : Node
-{
-    //[SerializeField]
-    protected List<Node> m_childNodes = new List<Node>();
-    public List<Node> ChildNodes => m_childNodes;
-
-    public BTSequence AddChild()
-    {
-        m_childNodes = ChildNode;
-        return this;
-    }
-
-    public override NodeState Result()
-    {
-        bool m_isRun = false;
-        for (int i = 0; i < m_childNodes.Count; i++)
-        {
-            switch (m_childNodes[i].Result())
-            {
-                case NodeState.Running:
-                    m_isRun = true;
-                    continue;
-                case NodeState.Success:
-                    continue;
-                case NodeState.Failure:
-                    m_currentState = NodeState.Failure;
-                    return CurrentState;
-                default:
-                    break;
-            }
-        }
-        if (m_isRun)
-        {
-            m_currentState = NodeState.Running;
-            return CurrentState;
-        }
-        else
-        {
-            m_currentState = NodeState.Success;
-            return CurrentState;
-        }
-    }
-}
-
-[System.Serializable]
-public class BTRepeater : Node
-{
-    protected List<Node> m_childNodes = new List<Node>();
-    public List<Node> ChildNodes => m_childNodes;
-
-    public BTRepeater AddChild()
-    {
-        m_childNodes = ChildNode;
-        return this;
-    }
-
-    public override NodeState Result()
-    {
-        var m_node = m_childNodes[0];
-
-        if (m_currentState == NodeState.Running)
-        {
-            switch (m_node.Result())
-            {
-                case NodeState.Running:
-                    m_currentState = NodeState.Running;
-                    return CurrentState;
-                case NodeState.Success:
-                    m_currentState = NodeState.Success;
-                    return CurrentState;
-                case NodeState.Failure:
-                    m_currentState = NodeState.Failure;
-                    return CurrentState;
-                default:
-                    return CurrentState;
-            }
-        }
-        else
+        public virtual NodeState Result()
         {
             return m_currentState;
         }
-    }
-}
 
-public class BTInverter: Node
-{
-    protected List<Node> m_childNodes = new List<Node>();
-    public List<Node> ChildNodes => m_childNodes;
-
-    public BTInverter AddChild()
-    {
-        m_childNodes = ChildNode;
-        return this;
-    }
-
-    public override NodeState Result()
-    {
-        var child = m_childNodes[0];
-
-        switch (child.Result())
+        public virtual Node GetNode()
         {
-            case NodeState.Running:
+            return this;
+        }
+
+        public List<Node> SetNode()
+        {
+            return m_childNode;
+        }
+    }
+
+    public enum NodeState
+    {
+        Running,
+        Success,
+        Failure
+    }
+
+    public class BTAction : Node
+    {
+        public override NodeState Result()
+        {
+            return Act();
+        }
+
+        /// <summary>実行内容</summary>
+        /// <returns>baseはFailure</returns>
+        protected virtual NodeState Act()
+        {
+            return NodeState.Failure;
+        }
+    }
+
+    public class BTConditional : Node
+    {
+        public override NodeState Result()
+        {
+            return Check();
+        }
+
+        /// <summary>条件判定</summary>
+        /// <returns>baseはFailure</returns>
+        protected virtual NodeState Check()
+        {
+            return NodeState.Failure;
+        }
+    }
+
+    public class BTSelector : Node
+    {
+        protected List<Node> m_childNodes = new List<Node>();
+        public List<Node> ChildNodes => m_childNodes;
+
+        public BTSelector AddChild()
+        {
+            m_childNodes = ChildNode;
+            return this;
+        }
+
+        public override NodeState Result()
+        {
+            for (int i = 0; i < m_childNodes.Count; i++)
+            {
+                switch (m_childNodes[i].Result())
+                {
+                    case NodeState.Running:
+                        m_currentState = NodeState.Running;
+                        return CurrentState;
+                    case NodeState.Success:
+                        m_currentState = NodeState.Success;
+                        return CurrentState;
+                    case NodeState.Failure:
+                        continue;
+                    default:
+                        break;
+                }
+            }
+            m_currentState = NodeState.Failure;
+            return CurrentState;
+        }
+    }
+
+    public class BTSequence : Node
+    {
+        protected List<Node> m_childNodes = new List<Node>();
+        public List<Node> ChildNodes => m_childNodes;
+
+        public BTSequence AddChild()
+        {
+            m_childNodes = ChildNode;
+            return this;
+        }
+
+        public override NodeState Result()
+        {
+            bool m_isRun = false;
+            for (int i = 0; i < m_childNodes.Count; i++)
+            {
+                switch (m_childNodes[i].Result())
+                {
+                    case NodeState.Running:
+                        m_isRun = true;
+                        continue;
+                    case NodeState.Success:
+                        continue;
+                    case NodeState.Failure:
+                        m_currentState = NodeState.Failure;
+                        return CurrentState;
+                    default:
+                        break;
+                }
+            }
+            if (m_isRun)
+            {
                 m_currentState = NodeState.Running;
                 return CurrentState;
-            case NodeState.Success:
-                m_currentState = NodeState.Failure;
-                return CurrentState;
-            case NodeState.Failure:
+            }
+            else
+            {
                 m_currentState = NodeState.Success;
                 return CurrentState;
-            default:
-                return CurrentState;
+            }
+        }
+    }
+
+    public class BTRepeater : Node
+    {
+        protected List<Node> m_childNodes = new List<Node>();
+        public List<Node> ChildNodes => m_childNodes;
+
+        public BTRepeater AddChild()
+        {
+            m_childNodes = ChildNode;
+            return this;
+        }
+
+        public override NodeState Result()
+        {
+            var m_node = m_childNodes[0];
+
+            if (m_currentState == NodeState.Running)
+            {
+                switch (m_node.Result())
+                {
+                    case NodeState.Running:
+                        m_currentState = NodeState.Running;
+                        return CurrentState;
+                    case NodeState.Success:
+                        m_currentState = NodeState.Success;
+                        return CurrentState;
+                    case NodeState.Failure:
+                        m_currentState = NodeState.Failure;
+                        return CurrentState;
+                    default:
+                        return CurrentState;
+                }
+            }
+            else
+            {
+                return m_currentState;
+            }
+        }
+    }
+
+    public class BTInverter : Node
+    {
+        protected List<Node> m_childNodes = new List<Node>();
+        public List<Node> ChildNodes => m_childNodes;
+
+        public BTInverter AddChild()
+        {
+            m_childNodes = ChildNode;
+            return this;
+        }
+
+        public override NodeState Result()
+        {
+            var child = m_childNodes[0];
+
+            switch (child.Result())
+            {
+                case NodeState.Running:
+                    m_currentState = NodeState.Running;
+                    return CurrentState;
+                case NodeState.Success:
+                    m_currentState = NodeState.Failure;
+                    return CurrentState;
+                case NodeState.Failure:
+                    m_currentState = NodeState.Success;
+                    return CurrentState;
+                default:
+                    return CurrentState;
+            }
         }
     }
 }
+
+
+
 
 //root
 ////Sequence(子を順に実行し初めに失敗した時点でfalse,全て成功ならtrue)
--- Assets/Scripts/BehaviorTree/Root.cs
+++ Assets/Scripts/BehaviorTree/Root.cs
@@ -1,34 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using BehaviorTree;
 
 public class Root : MonoBehaviour
 {
     [SerializeReference, SubclassSelector]
     Node m_myChildNode;
 
-    List<Node> m_nodes = new List<Node>();
-
     void Start()
     {
-        m_nodes = m_myChildNode.ChildNode;
-
         if (m_myChildNode != null)
         {
             Debug.Log($"子は{m_myChildNode}");
             m_myChildNode.GetNode();
-            if (m_nodes != null)
-            {
-                for (int i = 0; i < m_nodes.Count; i++)
-                {
-                    Debug.Log($"{i + 1}番目の孫は{m_nodes[i]}");
-                }
-                Debug.Log(m_myChildNode.Result());
-            }
-            else
-            {
-                Debug.Log("孫がない");
-            }
+            Debug.Log(m_myChildNode.Result());
         }
         else
         {
