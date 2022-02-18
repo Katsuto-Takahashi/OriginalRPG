022a74c54eea1fe7ca998a86ea9f1f08217bd79e
DESKTOP-FTQ7RG4\vantan <katoon.katudon@gmail.com>
2022-02-18 21:43:44 +0900

    敵

--- Assets/Scripts/SlimeManager.cs
+++ Assets/Scripts/SlimeManager.cs
@@ -7,6 +7,18 @@ public class SlimeManager : Enemy
 {
     protected override void SetUp()
     {
+        HP.DistinctUntilChanged().Subscribe(_ => m_besm.Parameter(this));
+        AP.DistinctUntilChanged().Subscribe(_ => m_besm.Parameter(this));
+
+        MaxHP.DistinctUntilChanged().Subscribe(_ => m_besm.Parameter(this));
+        MaxAP.DistinctUntilChanged().Subscribe(_ => m_besm.Parameter(this));
+        Strength.DistinctUntilChanged().Subscribe(_ => m_besm.Parameter(this));
+        Defense.DistinctUntilChanged().Subscribe(_ => m_besm.Parameter(this));
+        MagicPower.DistinctUntilChanged().Subscribe(_ => m_besm.Parameter(this));
+        MagicResist.DistinctUntilChanged().Subscribe(_ => m_besm.Parameter(this));
+        Speed.DistinctUntilChanged().Subscribe(_ => m_besm.Parameter(this));
+        Intelligence.DistinctUntilChanged().Subscribe(_ => m_besm.Parameter(this));
+
         m_mesm.SetUP(m_animator, m_rigidbody, m_capsuleCollider, m_myTransform, m_param);
         m_besm.SetUP();
     }
