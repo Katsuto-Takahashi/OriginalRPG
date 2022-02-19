c2c7024a8d40c5eb0c69ebfe3d1b87e94fc20cf4
DESKTOP-FTQ7RG4\vantan <katoon.katudon@gmail.com>
2022-02-19 20:56:31 +0900

    skill関係を作成開始

---
+++ Assets/Scripts/Skill.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class Skill : MonoBehaviour
+{
+    [SerializeField]
+    SkillData m_skillData = null;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+    }
+
+    public SkillData GetSkill()
+    {
+        return m_skillData;
+    }
+
+    void GotSkill()
+    {
+    }
+}
---
+++ Assets/Scripts/SkillPanel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class SkillPanel : MonoBehaviour
+{
+    Character m_character;
+
+    List<Skill> skills = new List<Skill>();
+
+    void Start()
+    {
+        m_character = GetComponent<Character>();
+    }
+
+    void Update()
+    {
+    }
+
+    void SkillUpdate()
+    {
+        
+    }
+
+    void GetSkill(int index)
+    {
+        m_character.GetSkill(skills[index].GetSkill());
+    }
+
+    void GotSkill()
+    {
+    }
+}
