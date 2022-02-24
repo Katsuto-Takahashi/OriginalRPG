266c207449e97ae1c607bab454cfac57ac25be37
DESKTOP-FTQ7RG4\vantan <katoon.katudon@gmail.com>
2022-02-24 21:51:31 +0900

    メニュー用

---
+++ Assets/Scripts/MenuDisplay.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuDisplay : MonoBehaviour
+{
+    [SerializeField] GameObject m_menu = null;
+    private bool m_menuDisplay = false;
+    void Start()
+    {
+        m_menuDisplay = true;
+        m_menu.SetActive(m_menuDisplay);
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Optionsbutton"))
+        {
+            if (m_menuDisplay)
+            {
+                m_menuDisplay = false;
+                m_menu.SetActive(m_menuDisplay);
+            }
+            else
+            {
+                m_menuDisplay = true;
+                m_menu.SetActive(m_menuDisplay);
+            }
+        }
+    }
+}
