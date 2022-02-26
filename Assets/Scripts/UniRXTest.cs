1dcc5ce8266243bc85b73e79212574937d636fe8
DESKTOP-FTQ7RG4\vantan <katoon.katudon@gmail.com>
2022-02-26 18:50:43 +0900

    unirx

---
+++ Assets/Scripts/UniRXTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class UniRXTest : MonoBehaviour
+{
+    UniRXSample uniRX;
+    void Start()
+    {
+        uniRX = GetComponent<UniRXSample>();
+        uniRX.TestRX
+            //.Where(x => x is true)
+            .Subscribe(x => DoTest(x));
+    }
+    void DoTest(bool a)
+    {
+        Debug.Log(a);
+    }
+}
