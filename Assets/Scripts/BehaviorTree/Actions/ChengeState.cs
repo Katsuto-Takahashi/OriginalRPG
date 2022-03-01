354f0dfaf03224e8cc401db04f12fb848b348a23
DESKTOP-FTQ7RG4\vantan <katoon.katudon@gmail.com>
2022-03-01 20:31:50 +0900

    stateを変更するaction

---
+++ Assets/Scripts/BehaviorTree/Actions/ChengeState.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviorTree;
+
+public class ChengeState : BTAction
+{
+    [SerializeField]
+    MovementEnemyStateMachine m_mesm;
+
+    enum ActEvent : byte
+    {
+        Idle,
+        Walk,
+        Run,
+        Chase,
+        Jump,
+        Fall,
+        Land,
+        Fly,
+        Stop,
+        Dead
+    }
+
+    [SerializeField]
+    ActEvent m_state;
+
+    protected override NodeState Act()
+    {
+        m_mesm.Chenge((int)m_state);
+        return NodeState.Success;
+    }
+
+    public override Node GetNode()
+    {
+        return this;
+    }
+}
