ad1765d8b8a8088d4e27bd42f8b6b51de40ad73c
DESKTOP-FTQ7RG4\vantan <katoon.katudon@gmail.com>
2022-03-03 22:42:09 +0900

    メニューのscriptを変更

---
+++ Assets/Scripts/MenuUI.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuUI : UIController
+{
+    protected override void CommandSelectedAction()
+    {
+        base.CommandSelectedAction();
+    }
+}
