9bf1e9de5245d0df838d6957e4894499b4aec820
DESKTOP-FTQ7RG4\vantan <katoon.katudon@gmail.com>
2022-03-06 19:30:09 +0900

    敵

--- Assets/Scripts/SlimeManager.cs
+++ Assets/Scripts/SlimeManager.cs
@@ -20,7 +20,11 @@ public class SlimeManager : Enemy
         Intelligence.DistinctUntilChanged().Subscribe(_ => m_besm.Parameter(this));
 
         m_mesm.SetUP(m_animator, m_rigidbody, m_capsuleCollider, m_sphereCollider, m_myTransform, m_param);
-        m_besm.SetUP();
+        m_besm.SetUP(m_animator, m_hsl, m_param);
+
+        m_besm.Move.DistinctUntilChanged().Subscribe(_ => Move());
+        m_besm.Stop.DistinctUntilChanged().Subscribe(s => Stop(s));
+        m_besm.Look.DistinctUntilChanged().Subscribe(_ => Look());
     }
 
     protected override void OnUpdate()
@@ -32,5 +36,20 @@ public class SlimeManager : Enemy
     protected override void OnFixedUpdate()
     {
         m_mesm.OnFixedUpdate();
+    }
+
+    void Move()
+    {
+        m_mesm.Chenge(MovementEnemyStateMachine.ActEvent.Run);
+    }
+
+    void Stop(bool stop)
+    {
+        m_mesm.CanMove = !stop;
+    }
+
+    void Look()
+    {
+        m_mesm.SetTarget(m_besm.TargetObject);
     }
 }
