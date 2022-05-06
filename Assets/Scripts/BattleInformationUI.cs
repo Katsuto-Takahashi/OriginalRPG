f5ad970a990bb173d7553c1629e7470531039a21
DESKTOP-FTQ7RG4\vantan <katoon.katudon@gmail.com>
2022-05-06 22:22:47 +0900

    テキスト表示の修正

--- Assets/Scripts/BattleInformationUI.cs
+++ Assets/Scripts/BattleInformationUI.cs
@@ -9,16 +9,6 @@ public class BattleInformationUI : MonoBehaviour
     [SerializeField] Text m_criticalDamageText = null;
     [SerializeField] Text m_informationText = null;
     [SerializeField] Text m_levelText = null;
-
-    void Start()
-    {
-        ;
-    }
-
-    void Update()
-    {
-        
-    }
     
     public IEnumerator BattleUIDisplay(int damage, string attackedCharacter, CriticalCheck critical)
     {
@@ -29,9 +19,16 @@ public class BattleInformationUI : MonoBehaviour
             m_criticalDamageText.gameObject.SetActive(false);
         }
         m_damageText.gameObject.SetActive(true);
-        m_damageText.text += $"{attackedCharacter}は{damage}のダメージをうけた\n";
-        yield return new WaitForSeconds(2);
         m_damageText.text = "";
+        if (damage > 0)
+        {
+            m_damageText.text += $"{attackedCharacter}は{damage}のダメージをうけた\n";
+        }
+        else
+        {
+            m_damageText.text += $"{attackedCharacter}はダメージをうけなかった\n";
+        }
+        yield return new WaitForSeconds(2);
         m_damageText.gameObject.SetActive(false);
     }
     public IEnumerator BattleStartUI(string name, int enemiesNumber)
@@ -52,16 +49,15 @@ public class BattleInformationUI : MonoBehaviour
     public IEnumerator BattleFinishUI(int check)
     {
         m_informationText.gameObject.SetActive(true);
+        m_damageText.text = "";
         if (check < 1)
         {
             yield return new WaitForSeconds(1);
-            m_damageText.text = "";
             m_informationText.text = "しょうりした";
         }
         else if (check < 2)
         {
             yield return new WaitForSeconds(1);
-            m_damageText.text = "";
             m_informationText.text = "はいぼくした";
         }
         else
@@ -69,12 +65,12 @@ public class BattleInformationUI : MonoBehaviour
             m_informationText.text = "にげだした";
         }
         yield return new WaitForSeconds(3);
-        m_informationText.text = " ";
         m_informationText.gameObject.SetActive(false);
     }
     public IEnumerator BattleResultUI(int check)
     {
         m_informationText.gameObject.SetActive(true);
+        m_informationText.text = " ";
         if (check < 1)
         {
             m_informationText.text = "しょうりした";
@@ -84,12 +80,12 @@ public class BattleInformationUI : MonoBehaviour
             m_informationText.text = "はいぼくした";
         }
         yield return new WaitForSeconds(3);
-        m_informationText.text = " ";
         m_informationText.gameObject.SetActive(false);
     }
     public IEnumerator GetExpUI(int exp, string charaName, int level, bool levelUp)
     {
         m_levelText.gameObject.SetActive(true);
+        m_levelText.text = " ";
         m_levelText.text = $"{exp}の経験値を手に入れた";
         yield return new WaitForSeconds(1);
         m_levelText.text = " ";
@@ -100,10 +96,4 @@ public class BattleInformationUI : MonoBehaviour
         }
         m_levelText.gameObject.SetActive(false);
     }
-}
-
-public enum CriticalCheck
-{
-    normal,
-    critical
 }
