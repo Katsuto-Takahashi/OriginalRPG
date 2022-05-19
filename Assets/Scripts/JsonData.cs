988c300a5bbec99f65a52e9ff94b71af5457696c
DESKTOP-FTQ7RG4\vantan <katoon.katudon@gmail.com>
2022-05-19 20:24:48 +0900

    scriptをjsonのデータをもつものに変更した

---
+++ Assets/Scripts/JsonData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class JsonData
+{
+    public string name;
+    public int hp;
+    public int maxhp;
+    public int ap;
+    public int maxap;
+    public int strength;
+    public int defence;
+    public int magicpower;
+    public int magicresist;
+    public int luck;
+    public int speed;
+    public int level;
+    public int skillpoint;
+    public int nowexp;
+    public int totalexp;
+    public int nextexp;
+    public int[] skillindex;
+}
