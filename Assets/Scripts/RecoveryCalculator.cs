1bb02ec23a5289df867228314adee0323fdd127c
DESKTOP-FTQ7RG4\vantan <katoon.katudon@gmail.com>
2022-07-10 01:27:20 +0900

    回復の機能を作成した

---
+++ Assets/Scripts/RecoveryCalculator.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecoveryCalculator
+{
+    int CalculateRecovery(int luck, int param, int standardValue, SkillData skillData)
+    {
+        var power = skillData.SkillPower / 100f;
+        float heal = param * standardValue * (Random.Range(0, luck) + 1);
+        return (int)(power * heal * skillData.SkillMagnification);
+    }
+
+    public int Recovery(Character character, SkillData skillData, int standardValue)
+    {
+        int param = skillData.Type == SkillType.Physical ? character.Strength.Value : character.MagicPower.Value;
+        return CalculateRecovery(character.Luck.Value, param, standardValue, skillData);
+    }
+
+    public int Recovery(Enemy enemy, SkillData skillData, int standardValue)
+    {
+        int param = skillData.Type == SkillType.Physical ? enemy.Strength.Value : enemy.MagicPower.Value;
+        return CalculateRecovery(enemy.Luck.Value, param, standardValue, skillData);
+    }
+}
