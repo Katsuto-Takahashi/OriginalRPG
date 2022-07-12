36ad5d6970369a1dd35a78e250b8dcbf00d35127
DESKTOP-FTQ7RG4\vantan <katoon.katudon@gmail.com>
2022-07-12 21:57:55 +0900

    バトル中のuiの処理を修正した

--- Assets/Scripts/UI/BattleInformationUI.cs
+++ Assets/Scripts/UI/BattleInformationUI.cs
@@ -5,32 +5,69 @@ using UnityEngine.UI;
 
 public class BattleInformationUI : MonoBehaviour
 {
-    [SerializeField] Text m_damageText = null;
-    [SerializeField] Text m_criticalDamageText = null;
-    [SerializeField] Text m_informationText = null;
-    [SerializeField] Text m_levelText = null;
-    
-    public IEnumerator BattleUIDisplay(int damage, string attackedCharacter, CriticalCheck critical)
+    Text m_damageText;
+    Text m_criticalText;
+    Text m_informationText;
+    Text m_levelText;
+
+    public void SetText(Text damageText, Text criticalText, Text informationText, Text levelText)
     {
-        if (critical == CriticalCheck.Critical)
-        {
-            m_criticalDamageText.gameObject.SetActive(true);
-            yield return new WaitForSeconds(0.5f);
-            m_criticalDamageText.gameObject.SetActive(false);
-        }
+        m_damageText = damageText;
+        m_criticalText = criticalText;
+        m_informationText = informationText;
+        m_levelText = levelText;
+    }
+
+    public IEnumerator CriticalHit()
+    {
+        m_criticalText.gameObject.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        m_criticalText.gameObject.SetActive(false);
+    }
+
+    public IEnumerator DamageUIDisplay(int damage, string characterName)
+    {
         m_damageText.gameObject.SetActive(true);
-        m_damageText.text = "";
+        m_damageText.text = " ";
         if (damage > 0)
         {
-            m_damageText.text += $"{attackedCharacter}は{damage}のダメージをうけた\n";
+            m_damageText.text += $"{characterName}は{damage}のダメージをうけた\n";
         }
         else
         {
-            m_damageText.text += $"{attackedCharacter}はダメージをうけなかった\n";
+            m_damageText.text += $"{characterName}はダメージをうけなかった\n";
         }
         yield return new WaitForSeconds(2);
         m_damageText.gameObject.SetActive(false);
     }
+    
+    public IEnumerator RecoveryUIDisplay(int heal, string characterName, HealPoint healPoint)
+    {
+        m_damageText.gameObject.SetActive(true);
+        m_damageText.text = " ";
+        string text;
+
+        switch (healPoint)
+        {
+            case HealPoint.HP:
+                text = $"{characterName}のHPが{heal}かいふくした\n";
+                break;
+            case HealPoint.AP:
+                text = $"{characterName}のAPが{heal}かいふくした\n";
+                break;
+            default:
+                text = "";
+                break;
+        }
+
+        if (heal > 0)
+        {
+            m_damageText.text += text;
+        }
+        yield return new WaitForSeconds(2);
+        m_damageText.gameObject.SetActive(false);
+    }
+
     public IEnumerator BattleStartUI(string name, int enemiesNumber)
     {
         m_informationText.gameObject.SetActive(true);
@@ -46,54 +83,52 @@ public class BattleInformationUI : MonoBehaviour
         m_informationText.text = " ";
         m_informationText.gameObject.SetActive(false);
     }
-    public IEnumerator BattleFinishUI(int check)
+
+    public IEnumerator BattleFinishUI(BattleResults result)
     {
         m_informationText.gameObject.SetActive(true);
-        m_damageText.text = "";
-        if (check < 1)
+        m_damageText.text = " ";
+        var resultText = "";
+        switch (result)
         {
-            yield return new WaitForSeconds(1);
-            m_informationText.text = "しょうりした";
+            case BattleResults.Win:
+                resultText = "しょうりした";
+                break;
+            case BattleResults.Lose:
+                resultText = "はいぼくした";
+                break;
+            case BattleResults.Escape:
+                resultText = "にげだした";
+                break;
+            default:
+                break;
         }
-        else if (check < 2)
-        {
-            yield return new WaitForSeconds(1);
-            m_informationText.text = "はいぼくした";
-        }
-        else
-        {
-            m_informationText.text = "にげだした";
-        }
+
+        yield return new WaitForSeconds(1);
+        m_informationText.text = resultText;
+
         yield return new WaitForSeconds(3);
         m_informationText.gameObject.SetActive(false);
     }
-    public IEnumerator BattleResultUI(int check)
-    {
-        m_informationText.gameObject.SetActive(true);
-        m_informationText.text = " ";
-        if (check < 1)
-        {
-            m_informationText.text = "しょうりした";
-        }
-        else
-        {
-            m_informationText.text = "はいぼくした";
-        }
-        yield return new WaitForSeconds(3);
-        m_informationText.gameObject.SetActive(false);
-    }
-    public IEnumerator GetExpUI(int exp, string charaName, int level, bool levelUp)
+
+    public IEnumerator LevelUpUI(Character character)
     {
         m_levelText.gameObject.SetActive(true);
         m_levelText.text = " ";
-        m_levelText.text = $"{exp}の経験値を手に入れた";
+
+        m_levelText.text += $"{character.Name.Value}のレベルが{character.Level.Value}にあがった";
+        yield return new WaitForSeconds(2);
+        character.LevelUP.Value = false;
+        m_levelText.gameObject.SetActive(false);
+    }
+
+    public IEnumerator GetExpUI(int exp, string charaName)
+    {
+        m_levelText.gameObject.SetActive(true);
+        m_levelText.text = " ";
+        m_levelText.text = $"{charaName}は{exp}の経験値を手に入れた";
         yield return new WaitForSeconds(1);
         m_levelText.text = " ";
-        if (levelUp)
-        {
-            m_levelText.text += $"{charaName}のレベルが{level}にあがった";
-            yield return new WaitForSeconds(2);
-        }
         m_levelText.gameObject.SetActive(false);
     }
 }
