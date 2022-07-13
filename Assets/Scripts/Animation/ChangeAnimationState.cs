91b86276ce6f75e91d821f5a830acd27a726a63b
DESKTOP-FTQ7RG4\vantan <katoon.katudon@gmail.com>
2022-07-14 00:12:59 +0900

    animation関係のscriptを整理した

--- Assets/Scripts/Animation/ChangeAnimationState.cs
+++ Assets/Scripts/Animation/ChangeAnimationState.cs
@@ -2,28 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AttackChecker : MonoBehaviour
+public class ChangeAnimationState : MonoBehaviour
 {
     Animator animator;
 
     AnimatorOverrideController controller;
 
     string overrideClipName = "Effect";
-
-    GameObject m_mine;
-
-    Skill m_skillData;
-
-    LayerMask m_target;
-
-    public void SetAttackParam(GameObject mine, Skill skill, LayerMask targetLayer)
-    {
-        m_mine = mine;
-        m_skillData = skill;
-        m_target = targetLayer;
-        ChangeClip(skill.SkillParameter.SkillAnimationClip);
-        AnimationController.Instance.PlayAnimation(animator, overrideClipName);
-    }
 
     public void SetAnim()
     {
@@ -49,14 +34,6 @@ public class AttackChecker : MonoBehaviour
         {
             //animator.Play(layerInfo[i].nameHash, i, layerInfo[i].normalizedTime);
             animator.Play(layerInfo[i].fullPathHash, i, layerInfo[i].normalizedTime);
-        }
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        if (((1 << other.gameObject.layer) & m_target) != 0)
-        {
-            other.GetComponent<ITakableDamage>().TakeDamage(BattleManager.Instance.Damage(m_mine, other.gameObject, m_skillData.SkillParameter));
         }
     }
 }
